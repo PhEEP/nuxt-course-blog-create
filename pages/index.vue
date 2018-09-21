4d75170e019352ee248bedf3a71da82bae3e4355
PhEEP <philippealmoore@gmail.com>
2018-09-21 15:53:55 -0700

    moneycat

--- pages/index.vue
+++ pages/index.vue
@@ -4,45 +4,24 @@
       <h1>Get the latest tech news</h1>
     </section>
     <section class="featured-posts">
-      <nuxt-link
-        :to="'/posts/' + 1"
-        class="post-preview"
-      >
-        <article>
-          <div
-            class="post-thumbnail"
-            style="background-image: url('https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg')"
-          />
-          <div
-            class="post-content"
-          >
-            <h1>Post Title</h1>
-            <p>Preview Text</p>
-          </div>
-        </article>
-      </nuxt-link>
-      <nuxt-link
-        :to="'/posts/' + 1"
-        class="post-preview"
-      >
-        <article>
-          <div
-            class="post-thumbnail"
-            style="background-image: url('https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg')"
-          />
-          <div
-            class="post-content"
-          >
-            <h1>Post Title</h1>
-            <p>Preview Text</p>
-          </div>
-        </article>
-      </nuxt-link>
+      <PostPreview
+        id="1"
+        thumbnail="../moneycat.jpg"
+      />
+      <PostPreview />
+      <PostPreview />
     </section>
   </div>
 </template>
 
 <script>
+import PostPreview from '@/components/Posts/PostPreview';
+
+export default {
+  components: {
+    PostPreview
+  }
+}
 </script>
 
 <style>
@@ -83,41 +62,5 @@
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
-}
-
-.post-preview {
-  border: 1px solid #ccc;
-  box-shadow: 0 2px 2px #ccc;
-  background-color: white;
-  width: 90%;
-}
-
-a {
-  text-decoration: none;
-  color: black;
-}
-
-@media (min-width: 850px) {
-  .post-preview {
-    width: 400px;
-    margin: 10px;
-  }
-}
-
-.post-thumbnail {
-  width: 100%;
-  height: 200px;
-  background-position: center;
-  background-size: cover;
-}
-
-.post-content {
-  padding: 10px;
-  text-align: center;
-}
-
-a:hover .post-content,
-a:active .post-content {
-  background-color: #ccc;
 }
 </style>
